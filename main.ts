c5b0d7e986a1866738f3a894a78d9418c3cd8e88
Huzaifa Naeem <159990985+huzaifanaeem1@users.noreply.github.com>
2024-03-30 18:40:43 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+let todos = [];
+
+
+while(true){
+let input =  await inquirer.prompt(
+    [
+        {
+            name: "Todoitem",
+            type:"input",
+            message:"Add todo item in listing"
+        },
+        {
+            name: "addMore",
+            type: 'list',
+            choices: ["yes","no"]
+        }       
+]);
+
+// output program
+
+const{Todoitem,addMore}= input;
+
+todos.push(Todoitem);
+if(addMore == "no"){
+    console.log("Todolist:")
+    for(let i=0; i<todos.length; i++)
+    {console.log(todos[i])}
+
+
+    break;
+}
+
+}
